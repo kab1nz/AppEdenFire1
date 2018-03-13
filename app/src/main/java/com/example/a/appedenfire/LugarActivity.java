be88e2767482f3e8934052bac3a6d0e3297c1b42
kab1nz <ismaelborregoinfor@gmail.com>
2018-03-13 01:06:09 +0100

    Modificando layout

--- app/src/main/java/com/example/a/appedenfire/LugarActivity.java
+++ app/src/main/java/com/example/a/appedenfire/LugarActivity.java
@@ -6,12 +6,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
 
-import com.example.a.appedenfire.objetos.Habitacion;
 import com.example.a.appedenfire.objetos.Lugar;
-import com.example.a.appedenfire.objetos.LugarListAdapter;
 import com.example.a.appedenfire.objetos.ReservaListAdapter;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
@@ -26,7 +22,6 @@ public class LugarActivity extends AppCompatActivity {
     private static final String TAGLOG ="Firebase -->" ;
     private List<Lugar> lugarList;
     private ReservaListAdapter lugarListAdapter;
-    private FirebaseFirestore firebaseFirestore;
 
 
     @Override
@@ -41,36 +36,21 @@ public class LugarActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         lugarList = new ArrayList<>();
-        final LugarListAdapter lugarListAdapter = new LugarListAdapter(getApplicationContext(), lugarList);
 
         RecyclerView recycler = (RecyclerView) findViewById(R.id.recyclerViewLugar);
         recycler.setHasFixedSize(true);
         recycler.setLayoutManager(new LinearLayoutManager(this));
         recycler.setAdapter(lugarListAdapter);
-        firebaseFirestore= FirebaseFirestore.getInstance();
-        firebaseFirestore.collection("Lugar").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
-                if(e!=null){
-                    Log.d(TAGLOG,"Error: "+e.getMessage());
-                }
 
-                for (DocumentChange doc : documentSnapshots.getDocumentChanges()){
-
-                    if(doc.getType()==DocumentChange.Type.ADDED){
-                        String userId = doc.getDocument().getId();
-
-                        Lugar hab = doc.getDocument().toObject(Lugar.class).widthId(userId);
-
-                        lugarList.add(hab);
-                        lugarListAdapter.notifyDataSetChanged();
-                    }
-
-                }
-
-
-            }
-        });
+        lugarList = new ArrayList<>();
+        lugarList.add(
+                new Lugar(
+                        1,
+                        "Las Negras)",
+                        "Buen sitio",
+                        4.3,
+                        R.drawable.lasnegras));
+        )
 
     }
 }
